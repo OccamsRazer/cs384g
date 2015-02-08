c7f44bb167140a57fb33748f7611ef14ee9fff25
Jared Croy <jcroy2010@yahoo.com>
2015-02-08 16:42:22 -0600

    added triangle brush

--- impressionist/impressionistDoc.cpp
+++ impressionist/impressionistDoc.cpp
@@ -17,6 +17,7 @@
 
 // Include individual brush headers here.
 #include "pointBrush.h"
+#include "triangleBrush.h"
 
 
 #define DESTROY(p)	{  if ((p)!=NULL) {delete [] p; p=NULL; } }
@@ -37,6 +38,7 @@ ImpressionistDoc::ImpressionistDoc()
 	ImpBrush::c_pBrushes	= new ImpBrush* [ImpBrush::c_nBrushCount];
 
 	ImpBrush::c_pBrushes[BRUSH_POINTS]	= new PointBrush( this, "Points" );
+	ImpBrush::c_pBrushes[BRUSH_TRIANGLES]	= new TriangleBrush( this, "Triangles" );
 
 	// Note: You should implement these 5 brushes.  They are set the same (PointBrush) for now
 	ImpBrush::c_pBrushes[BRUSH_LINES]				
